0c3f450192b48c138d0ae5dc22267e0addf73c91
Sachin Rathnayake <sachirathnayake9@gmail.com>
2026-02-20 09:48:29 +0530

    Feat: change the review.ts schema

--- src/sanity/schemaTypes/review.ts
+++ src/sanity/schemaTypes/review.ts
@@ -1,35 +1,37 @@
-export default {
+import { defineField, defineType } from 'sanity'
+
+export const review = defineType({
   name: 'review',
   title: 'Reviews',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'product',
       title: 'Product',
       type: 'reference',
       to: [{ type: 'product' }],
-    },
-    {
+    }),
+    defineField({
       name: 'user',
       title: 'User Name',
       type: 'string', 
-    },
-    {
+    }),
+    defineField({
       name: 'rating',
       title: 'Rating (1-5)',
       type: 'number',
-      validation: (Rule: any) => Rule.min(1).max(5),
-    },
-    {
+      validation: (Rule) => Rule.min(1).max(5),
+    }),
+    defineField({
       name: 'comment',
       title: 'Comment',
       type: 'text',
-    },
-    {
+    }),
+    defineField({
       name: 'createdAt',
       title: 'Created At',
       type: 'datetime',
       initialValue: () => new Date().toISOString(),
-    }
+    })
   ]
-}
+})
